9c9287eabcd1846eb4b8d302cfd2bcbf110c0b55
rizaldi <rizaldi354313@gmail.com>
2020-05-18 08:11:38 +0700

    a

--- application/models/Ajax_m.php
+++ application/models/Ajax_m.php
@@ -10,8 +10,8 @@ class Ajax_m extends CI_Model
     private function _get_datatables_query() {
 		$this->db->select('DISTINCT(waktu), id_stasiun, h2s, cs2, velocity, temperature');
 		$this->db->group_by('waktu'); 
-        $this->db->from('aqm_data');
         $this->db->where('id_stasiun', 'CEMS_RUM');
+        $this->db->from('aqm_data');
         $i = 0;
         foreach ($this->column_search as $item) { // loop column
             if(@$_POST['search']['value']) { // if datatable send POST for search
